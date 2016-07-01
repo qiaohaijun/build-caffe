d3d258dc5c73553f8e8fd33261a69dcd0be339b7
qiaohaijun <qiaohaijun@aliyun.com>
2016-07-01 16:35:56 +0800

    build caffe from src

---
+++ build-python.sh
@@ -0,0 +1,27 @@
+source ./config.sh
+
+#CURRENT_FILE=$(cd `dirname $0`;pwd)
+
+DEBUG "[${SRC_DIR}]"
+DEBUG "[${TARGET_DIR}]"
+DEBUG "[${PYTHON_WGET_LINK}]"
+DEBUG "[${PYTHON_TAR}]"
+
+if [ -f ${SRC_DIR}/${PYTHON_TAR} ]; then
+    WARN "${SRC_DIR}/${PYTHON_TAR} exist "
+else
+    INFO "downloading python 2.7.11"
+    wget ${PYTHON_WGET_LINK} -O ${SRC_DIR}/${PYTHON_TAR}  
+    INFO "python2.7.11 is done"
+fi
+
+## uncompress
+if [ -d ${SRC_DIR}/${PYTHON_FILE} ] ;then
+    WARN "${SRC_DIR}/${PYTHON_FILE} exist, will delete it "
+    rm -rf ${SRC_DIR}/${PYTHON_FILE}
+fi
+
+cd ${SRC_DIR}/; tar xvfz Python-2.7.11.tgz 
+
+##
+cd ${SRC_DIR}/${PYTHON_FILE}; ./configure --prefix=${TARGET_DIR}; make -j ; make install
