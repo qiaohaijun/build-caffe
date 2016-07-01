e355ba13b69e254fbadc9468214efcfc3519ccd7
qiaohaijun <qiaohaijun@aliyun.com>
2016-07-01 20:15:24 +0800

    add CXX CC

--- my_env.sh
+++ my_env.sh
@@ -6,3 +6,7 @@ export LD_LIBRARY_PATH=/search/odin/cudnn_v4007/cuda/lib64/:$LD_LIBRARY_PATH
 
 # PATH
 export PATH=/search/odin/caffe-deps/bin/:$PATH
+unset CXX
+unset CC
+export CXX="g++ -std=c++11"
+export CC="gcc -std=c99"
