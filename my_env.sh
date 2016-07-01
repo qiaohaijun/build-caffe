4424cbf94f16773b04ce6ea9950c0ef3bacacbf9
qiaohaijun <qiaohaijun@aliyun.com>
2016-07-01 15:25:00 +0800

    my env config LD_LIBRARY_PATH etc.. global

---
+++ my_env.sh
@@ -0,0 +1,8 @@
+# LD_LIBRARY_PATH
+unset LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/search/odin/caffe-deps/lib/:$LD_LIBRARY_PATH
+# cudnn
+export LD_LIBRARY_PATH=/search/odin/cudnn_v4007/cuda/lib64/:$LD_LIBRARY_PATH
+
+# PATH
+export PATH=/search/odin/caffe-deps/bin/:$PATH
